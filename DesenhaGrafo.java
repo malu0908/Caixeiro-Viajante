d2d1031548d35968aef84cc659c218b31105d9d9
malu0908 <40873568+malu0908@users.noreply.github.com>
2019-05-20 14:50:32 -0300

    Update DesenhaGrafo.java

--- DesenhaGrafo.java
+++ DesenhaGrafo.java
@@ -5,43 +5,62 @@ import java.awt.*;
 import java.util.concurrent.TimeUnit;
 
 public class DesenhaGrafo extends JPanel {
-	public int[] rota; // rota completa para imprimir
-	public double[][] matriz; // matriz das posicoes, aquele arquivo de entrada
+	public int[] seqCidade;
+	public double[][] matriz;
+	public tb17 score;
 
-	public DesenhaGrafo(int[] rota, double[][] matrizPontos) {
-		this.rota = rota; // inicializacao recebendo a rota
-		this.matriz = matrizPontos; // inicializacao rebendo o matrizEntrada
+	public DesenhaGrafo(int[] seqCidade, double[][] matrizPontos) {
+		this.seqCidade = seqCidade;
+		this.matriz = matrizPontos;
 	}
 
-	@Override
 	public void paintComponent(Graphics grafo) {
 		super.paintComponent(grafo);
 		this.setBackground(Color.black);
 
+		int escala = 6;
 		int j = 1, i = 0;
 		grafo.setColor(Color.cyan);
-		grafo.fillOval((int) (this.matriz[this.rota[i]][0] * 5), (int) (this.matriz[this.rota[i]][1] * 5), 20, 20);
-		for (i = 1; i < rota.length; i++) { //desenha as cidades
-			if(i % 2 == 0)
-				grafo.setColor(Color.red); // senao colore de vermelho
-			else
-				grafo.setColor(Color.blue); // senao colore de vermelho	
-			grafo.fillOval((int) (this.matriz[this.rota[i]][0] * 5), (int) (this.matriz[this.rota[i]][1] * 5), 20, 20);
+		grafo.fillOval((int) (this.matriz[this.seqCidade[i]][0] * escala - 7),
+				(int) (this.matriz[this.seqCidade[i]][1] * escala - 7), 15, 15);
+		grafo.setColor(Color.black);
+		String x = Integer.toString(this.seqCidade[0]);
+		grafo.drawString(x, (int) (this.matriz[this.seqCidade[i]][0] * escala),
+				(int) (this.matriz[this.seqCidade[i]][1] * escala));
+		for (i = 1; i < seqCidade.length; i++) {
+			grafo.setColor(Color.red);
+			grafo.fillOval((int) (this.matriz[this.seqCidade[i]][0] * escala - 7),
+					(int) (this.matriz[this.seqCidade[i]][1] * escala - 7), 15, 15);
+			grafo.setColor(Color.white);
+			x = Integer.toString(this.seqCidade[i]);
+			grafo.drawString(x, (int) (this.matriz[this.seqCidade[i]][0] * escala),
+					(int) (this.matriz[this.seqCidade[i]][1] * escala));
 		}
-		for (i = 0; i < rota.length && j < rota.length; i++) {
-			grafo.setColor(Color.white); // colore as arestas de preto
-			grafo.drawLine((int) (this.matriz[this.rota[i]][0] * 5), (int) (this.matriz[this.rota[i]][1] * 5),
-					(int) (this.matriz[rota[j]][0] * 5), (int) (this.matriz[this.rota[j]][1] * 5));
+		for (i = 0; i < seqCidade.length && j < seqCidade.length; i++) {
+			grafo.setColor(Color.white);
+			grafo.drawLine((int) (this.matriz[this.seqCidade[i]][0] * escala),
+					(int) (this.matriz[this.seqCidade[i]][1] * escala), (int) (this.matriz[seqCidade[j]][0] * escala),
+					(int) (this.matriz[this.seqCidade[j]][1] * escala));
+			grafo.setColor(Color.white);
+			x = Integer.toString(this.seqCidade[i]);
 			j++;
 		}
-		
-		//colore o ultimo vertice de magenta
+
 		grafo.setColor(Color.MAGENTA);
-		grafo.fillOval((int) (this.matriz[this.rota[j - 1]][0] * 5), (int) (this.matriz[this.rota[j - 1]][1] * 5), 20, 20);
-		
-		//colore o vertice que interliga o primeiro vertice com o ultimo de amarelo
-		grafo.setColor(Color.yellow);
-		grafo.drawLine((int) (this.matriz[this.rota[j - 1]][0] * 5), (int) (this.matriz[this.rota[j - 1]][1] * 5),
-				(int) (this.matriz[this.rota[0]][0] * 5), (int) (this.matriz[this.rota[0]][1] * 5));
+		grafo.fillOval((int) (this.matriz[this.seqCidade[j - 1]][0] * escala - 7),
+				(int) (this.matriz[this.seqCidade[j - 1]][1] * escala - 7), 15, 15);
+		grafo.setColor(Color.white);
+		x = Integer.toString(this.seqCidade[i]);
+		grafo.drawString(x, (int) (this.matriz[this.seqCidade[j - 1]][0] * escala),
+				(int) (this.matriz[this.seqCidade[j - 1]][1] * escala));
+
+		grafo.setColor(Color.pink);
+		grafo.drawLine((int) (this.matriz[this.seqCidade[j - 1]][0] * escala),
+				(int) (this.matriz[this.seqCidade[j - 1]][1] * escala),
+				(int) (this.matriz[this.seqCidade[0]][0] * escala), (int) (this.matriz[this.seqCidade[0]][1] * escala));
+
+		grafo.setColor(Color.white);
+		x = Double.toString(this.score.melhorScore);
+		grafo.drawString(x, 600, 100);
 	}
 }
